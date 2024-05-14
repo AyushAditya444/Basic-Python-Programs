a76f1d6dd59bb3cb59437f7280397cbffe1b1fb2
AyushAditya444 <ayushaditya444@gmail.com>
2024-05-14 12:24:08 +0530

    Add files via upload

---
+++ Variables.py
@@ -0,0 +1,12 @@
+#1st Type Variable (Integer)
+x=20
+print(x)
+
+#2nd Type Variable (String)
+First_Name="Ayush"
+Second_Name="Aditya"
+print(First_Name,Second_Name)
+
+#3rd Type Variable (Boolean)
+I_Am_A_Boy=True
+print(I_Am_A_Boy)
