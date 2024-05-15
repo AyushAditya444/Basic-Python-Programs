41ea9e1092ae04ccf8f74c1cb86228c2609221d4
AyushAditya444 <ayushaditya444@gmail.com>
2024-05-15 20:44:35 +0530

    Add files via upload

---
+++ Arithmetic Operators.py
@@ -0,0 +1,33 @@
+X=10
+Y=20
+A=2
+B=3
+
+#Addition
+print(X+Y)
+
+#Subtraction
+print(Y-X)
+
+#Multiplication
+print(X*Y)
+
+#Float Division
+print(Y/X)
+
+#Interger Division
+print(X//Y)
+
+#Exponent
+print(A**B)
+
+#Modulus
+print(X%Y)
+
+X+=3
+Y-=3
+print(X,Y)
+
+#Opertor Precedence
+print("Value of '2+3*2**2/2-5' is :-")
+print(10+3*2**2/2-5)
