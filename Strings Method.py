b562a69b1b874cc964a1b16f0f501fc029f30948
AyushAditya444 <ayushaditya444@gmail.com>
2024-05-15 20:44:12 +0530

    Add files via upload

---
+++ Strings Method.py
@@ -0,0 +1,28 @@
+Name="Hi I am Ayush Aditya."
+
+#Print Length of String
+print(len(Name))
+
+#Print String in Upper Case
+print(Name.upper())
+
+#Print String in Lower Case
+print(Name.lower())
+
+#Print String in Title Case
+print(Name.title())
+
+#Finding A Letter in String
+print(Name.find("a"))
+
+#Finding A Word in String
+print(Name.find("Ayush"))
+
+#Replace A Letter in String
+print(Name.replace("A","a"))
+
+#Replace A Word in String
+print(Name.replace("Ayush Aditya","Molu"))
+
+#Finding A Word in String
+print("Ayush" in Name)
