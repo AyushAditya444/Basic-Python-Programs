7fbce4e0df12f2c6dadee81a3affc31a9e7b40ab
AyushAditya444 <ayushaditya444@gmail.com>
2024-05-15 20:43:41 +0530

    Add files via upload

---
+++ Array String.py
@@ -0,0 +1,19 @@
+Name="Hi I am Ayush Aditya."
+
+#Print Full Text
+print(Name)
+
+#Print First Character
+print(Name[0])
+
+#Print Last Character
+print(Name[-1])
+
+#Print First 5 Characters
+print(Name[0:5])
+
+#Print Last 5 Characters
+print(Name[-5:])
+
+#Print Every 2nd Character
+print(Name[::2])
