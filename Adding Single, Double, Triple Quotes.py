696278449334b9cc8894858a31ea43959e0c1363
AyushAditya444 <ayushaditya444@gmail.com>
2024-05-15 20:43:23 +0530

    Add files via upload

---
+++ Adding Single, Double, Triple Quotes.py
@@ -0,0 +1,12 @@
+#No Extra Quotes
+String1="Hi I am Ayush Aditya."
+
+#Single Quotes
+String2='Hi I am "Ayush Aditya".'
+
+#Double Quotes
+String3=''''Hi' I am "Ayush Aditya".'''
+
+print(String1)
+print(String2)
+print(String3)
