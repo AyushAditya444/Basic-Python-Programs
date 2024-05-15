30cd7bac890b2b4ebd6cc40b500a91b081150eb7
AyushAditya444 <ayushaditya444@gmail.com>
2024-05-15 20:45:08 +0530

    Add files via upload

---
+++ Math Function.py
@@ -0,0 +1,12 @@
+import math
+
+X=2.9
+Y=-9.2
+print(round(X))
+print(abs(Y))
+print(math.ceil(X))
+print(math.floor(X))
+print(math.sqrt(X))
+print(math.ceil(Y))
+print(math.floor(Y))
+print(math.sqrt(abs(Y)))
