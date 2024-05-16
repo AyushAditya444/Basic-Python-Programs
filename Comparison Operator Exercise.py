7778323ac1c6113974a31ddd31ce00faceb2aae2
AyushAditya444 <ayushaditya444@gmail.com>
2024-05-16 17:00:07 +0530

    Add files via upload

---
+++ Comparison Operator Exercise.py
@@ -0,0 +1,7 @@
+Input1=input("What is Your Name: ")
+if len(Input1)>=50:
+    print("Your Name is Very Long")
+elif len(Input1)<=3:
+    print("Your Name is Very Short")
+else:
+    print("Your Name is Looks Good")
