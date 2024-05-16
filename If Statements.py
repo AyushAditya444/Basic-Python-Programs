904526015e0f615ebc53a089ab2fc226dd60abee
AyushAditya444 <ayushaditya444@gmail.com>
2024-05-16 16:58:28 +0530

    Add files via upload

---
+++ If Statements.py
@@ -0,0 +1,9 @@
+Weather=input("What's the weather like today? (Hot/Cold/Lovely): ")
+if Weather=="Hot":
+    print("It's a hot day")
+    print("Drink plenty of water")
+elif Weather=="Cold":
+    print("It's a cold day")
+    print("Wear warm clothes")
+else:
+    print("It's a lovely day")
