2a2adf79ecb2e9b3b57d2a4c4b241ad3c21bf4bf
AyushAditya444 <ayushaditya444@gmail.com>
2024-05-21 17:54:03 +0530

    Add files via upload

---
+++ For Loops.py
@@ -0,0 +1,17 @@
+for i in "Python":
+    print(i)
+print("New Line:-")
+for i in ["Ayush", "Aditya", "Molu"]:
+    print(i)
+print("New Line:-")
+for i in range(10):
+    print(i)
+print("New Line:-")
+for i in range(5,10):
+    print(i)
+print("New Line:-")
+for i in range(5,10):
+    print(i)
+print("New Line:-")
+for i in range(1,10,2):
+    print(i)
