bbdc4de24e638a67156f93499406378a1fbc701f
AyushAditya444 <ayushaditya444@gmail.com>
2024-05-24 17:28:28 +0530

    Add files via upload

---
+++ List.py
@@ -0,0 +1,25 @@
+Names=["Ayush", "Aditya", "Molu", "Ragini", "Rani"]
+
+#Print Full List
+print("Full List:")
+print(Names)
+
+#Print First Element
+print("First Element:")
+print(Names[0])
+
+#Print Last Element
+print("Last Element:")
+print(Names[-1])
+
+#Print First 3 Elements
+print("First 3 Elements:")
+print(Names[0:3])
+
+#Print Last 3 Elements
+print("Last 3 Elements:")
+print(Names[-3:])
+
+#Print Every 2nd Element
+print("Every 2nd Element:")
+print(Names[::2])
